973ee4ef50c1353fc952c9eacf1b1779905b02ae
Alex Rodionov <p0deje@gmail.com>
2019-01-10 15:39:17 +0600

    Remove OSS version of Options and replace it with the W3C version

--- rb/lib/selenium/webdriver/remote/bridge.rb
+++ rb/lib/selenium/webdriver/remote/bridge.rb
@@ -351,7 +351,7 @@ module Selenium
         #
 
         def options
-          @options ||= WebDriver::W3COptions.new(self)
+          @options ||= WebDriver::Options.new(self)
         end
 
         def add_cookie(cookie)
